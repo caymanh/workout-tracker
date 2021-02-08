a3e8e552167df138eabd60e1df08ee11982f7037
Cayman Heng <hengcayman@gmail.com>
2021-02-08 13:01:10 -0500

    updated naming convention in workout.js

--- models/workout.js
+++ models/workout.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
+const workoutSchema = new Schema({
   day: {
     type: Date,
     default: Date.now,
@@ -38,6 +38,6 @@ const WorkoutSchema = new Schema({
   ],
 });
 
-const Workout = mongoose.model("workout", WorkoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
