c216eaf15df821be6c4d745bff6ff8661b532085
Cayman Heng <hengcayman@gmail.com>
2021-02-08 13:00:41 -0500

    require path in html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,15 +1,16 @@
 const router = require("express").Router();
+const path = require('path');
 
 router.get("/exercise", (req, res) => {
-  res.sendFile("../public/exercise.html", { root: __dirname });
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
-  res.sendFile("../public/stats.html", { root: __dirname });
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 router.get("/", (req, res) => {
-  res.sendFile("../public/stats.html", { root: __dirname });
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = router;
