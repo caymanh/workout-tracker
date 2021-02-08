db28e92a8bcfd1673a1d137c093f2ef00836f585
Cayman Heng <hengcayman@gmail.com>
2021-02-08 10:17:04 -0500

    updated workout schema to link it with the exercise schema

--- models/workout.js
+++ models/workout.js
@@ -2,15 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  day: new Date().setDate(new Date().getDate() - 5),
+  day: {
+    type: Date,
+  },
   exercises: [
     {
-      type: String,
-      name: String,
-      duration: Number,
-      weight: Number,
-      reps: Number,
-      sets: Number,
+      type: Schema.Types.ObjectId,
+      ref: "Exercise",
     },
   ],
 });
