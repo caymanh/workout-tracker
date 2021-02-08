2816f399cdf49bd22a4707d4b8c4b3fb802da4a3
Cayman Heng <hengcayman@gmail.com>
2021-02-08 11:32:52 -0500

    updated models by combining exercise schema into workout schema

--- models/workout.js
+++ models/workout.js
@@ -4,15 +4,40 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
   day: {
     type: Date,
+    default: Date.now,
   },
   exercises: [
     {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise",
+      type: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
     },
   ],
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("workout", WorkoutSchema);
 
 module.exports = Workout;
